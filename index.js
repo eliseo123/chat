5102c358623114c01d6bd624e40d8abdb6616b5d
eliseo montefalcon <eli.montefalcon@myxlog.com>
2017-06-13 14:37:05 +0800

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var app = require('express')();
+/*var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = process.env.port || 1337;
@@ -16,3 +16,27 @@ io.on('connection', function (socket) {
       io.emit('chat message', msg);
   });
 });
+
+*/
+
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
