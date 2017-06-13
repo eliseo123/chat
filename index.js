d3be9b85734e33f67de1287a6031b0e9d4b207a5
eliseo montefalcon <eli.montefalcon@myxlog.com>
2017-06-13 13:55:10 +0800

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var port = process.env.port || 5000;
+var port = process.env.port || 1337;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
